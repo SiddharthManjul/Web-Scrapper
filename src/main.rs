a2ca99c011bac5484adfd3e8f45d6febb2c1a684
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-07-06 00:05:05 +0530

    Async Main Function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use scraper::{selector, Html, Selector};
+use printpdf::*;
+use tokio::join;
+use std::fs::File;
+use std::io::BufWriter;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "https://doc.rust-lang.org/book/";
+    let response = reqwest::get(url).await?.text().await?;
+
+    let document = Html::parse_document(&response);
+    let selector = Selector::parse("body").unwrap();
+
+    let mut text_content = String::new();
+
+    for element in document.select(&selector) {
+        text_content.push_str(&element.text().collect::<Vec<_>>().join(" "));
+    }
+
+    Ok(())
 }
